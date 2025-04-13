095444d96c2c0be436f72a3965631a678b971aec
tomaszmaslon <tomasz.maslon@gmail.com>
2025-04-13 14:37:12 +0200

    Showcasing allignment and sinple composables on screen

--- app/src/main/java/pl/farmaprom/trainings/contactsapp/ui/MainActivity.kt
+++ app/src/main/java/pl/farmaprom/trainings/contactsapp/ui/MainActivity.kt
@@ -3,13 +3,22 @@ package pl.farmaprom.trainings.contactsapp.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import pl.farmaprom.trainings.contactsapp.ui.theme.ContactsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +27,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             ContactsAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.surface
-                ) {
-                    Greeting("Android")
+                ) { padding ->
+                    Greeting(
+                        modifier = Modifier.padding(padding),
+                        name = "Android"
+                    )
                 }
             }
         }
@@ -30,14 +41,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(
+    modifier: Modifier,
+    name: String
+) {
+    Row (
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "Hello $name!",
+            style = MaterialTheme.typography.headlineLarge
+        )
+        Text(
+            modifier = modifier
+                .padding(top = 8.dp)
+                .clickable { },
+            style = MaterialTheme.typography.headlineMedium,
+            text = "Having fun?"
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    device = Devices.PIXEL_7_PRO
+)
 @Composable
 fun DefaultPreview() {
     ContactsAppTheme {
-        Greeting("Android")
+        Greeting(
+            modifier = Modifier,
+            name = "Android"
+        )
     }
 }
+
+//
+//@Preview(
+//    showBackground = true,
+//    device = Devices.NEXUS_5
+//)
+//@Composable
+//fun DefaultPreviewNexus() {
+//    ContactsAppTheme {
+//        Greeting(
+//            modifier = Modifier,
+//            name = "Android"
+//        )
+//    }
+//}
+//
