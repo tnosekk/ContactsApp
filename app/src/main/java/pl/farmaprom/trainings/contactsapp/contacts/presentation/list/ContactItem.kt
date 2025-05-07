a3b1013289fbec8251253d095f54d4329687b59b
tnosekk <nosektomek@gmail.com>
2025-05-07 11:49:35 +0200

    zad1

--- app/src/main/java/pl/farmaprom/trainings/contactsapp/contacts/presentation/list/ContactItem.kt
+++ app/src/main/java/pl/farmaprom/trainings/contactsapp/contacts/presentation/list/ContactItem.kt
@@ -1,37 +1,36 @@
 package pl.farmaprom.trainings.contactsapp.contacts.presentation.list
 
-import android.media.Image
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.skydoves.landscapist.glide.GlideImage
 import pl.farmaprom.trainings.contactsapp.R
 import pl.farmaprom.trainings.contactsapp.ui.theme.ContactsAppTheme
-
-
-
+import pl.farmaprom.trainings.contactsapp.ui.theme.Dimens
 
 @Composable
 fun ContactItem(
-    modifier: Modifier = Modifier,
+    profileImageUrl: String,
     name: String,
-    isFavorite: Boolean,
+    isFavourite: Boolean = false,
+    modifier: Modifier = Modifier
 ) {
-
     Row(
         modifier = modifier
             .fillMaxWidth()
@@ -39,25 +38,52 @@ fun ContactItem(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Image(
-            modifier = modifier.size(36.dp),
-            painter = painterResource(R.drawable.face),
-            contentDescription = null
+
+        GlideImage(
+            imageModel = { profileImageUrl },
+            modifier = Modifier
+               .size(Dimens.contactListItemProfileImageSize)
+               .clip(CircleShape)
         )
+
+        Text(
+            text = name,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 12.dp)
+        )
+
         Icon(
-            imageVector = if (isFavorite) Icons.Filled.Star else Icons.Outlined.Star,
-            contentDescription = null
+            imageVector = if (isFavourite) Icons.Filled.Star else Icons.Outlined.Star,
+            contentDescription = if (isFavourite) "Favourite Contact" else "Not Favourite Contact",
+            tint = if (isFavourite) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
         )
-
     }
-
-
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun ContactItemPreview() {
     ContactsAppTheme {
-        ContactItem(name = "Joe Doe", isFavorite = true)
+        val sampleImageUrl = "https://raw.githubusercontent.com/kamilruchalaf/trainingassets/main/assets/face1.png"
+        ContactItem(
+            profileImageUrl = sampleImageUrl,
+            name = "Joe Doe",
+            isFavourite = true
+        )
     }
 }
+@Preview(showBackground = true)
+@Composable
+private fun ContactItemPreviewNotFavourite() {
+    ContactsAppTheme {
+        val sampleImageUrl = "https://raw.githubusercontent.com/kamilruchalaf/trainingassets/main/assets/face1.png"
+        ContactItem(
+            profileImageUrl = sampleImageUrl,
+            name = "Joe Noe",
+            isFavourite = false
+        )
+    }
+}
+
